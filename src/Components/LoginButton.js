963d3697309b371f83f3fc45bc278f1f93db30cf
kumesh1997 <tharindukumesh09@gmail.com>
2022-08-20 22:56:45 +0530

    Final with UI

--- src/Components/LoginButton.js
+++ src/Components/LoginButton.js
@@ -5,7 +5,7 @@ const LoginButton = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
     !isAuthenticated && (
-    <button onClick={() => loginWithRedirect()} className=" bg-green-400 p-2 bottom-2 shadow-lg rounded-full m-2 font-bold duration-200 active: translate-x-2 ">Log In</button>
+    <button onClick={() => loginWithRedirect()} className=" bg-green-400 p-2 bottom-2 shadow-lg rounded-full m-2 font-bold duration-100 hover:scale-105 active:scale-95 border-black border-2">Log In</button>
     )
   )
 }
--- src/Components/LogoutButton.js
+++ src/Components/LogoutButton.js
@@ -5,7 +5,7 @@ function LogoutButton() {
     const { logout, isAuthenticated } = useAuth0();
   return (
     isAuthenticated && (
-    <button onClick={() => logout()} className=" bg-red-500 p-2 bottom-2 shadow-lg rounded-full m-2 font-bold">Logout</button>
+    <button onClick={() => logout()} className=" bg-red-500 p-2 bottom-2 shadow-lg rounded-full m-2 font-bold border-black border-2 duration-100 hover:scale-105 active:scale-95">Logout</button>
     )
   )
 }
